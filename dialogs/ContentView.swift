88b3ca945a2e0475c2aa400aad3a3694fb6bea91
paigeshin <paigeshin1991@gmail.com>
2023-06-02 21:06:49 +0900

    init

---
+++ dialogs/ContentView.swift
@@ -0,0 +1,120 @@
+//
+//  ContentView.swift
+//  dialogs
+//
+//  Created by paige shin on 2023/06/02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Button {
+                PresentingViewController().presentPopup()
+            } label: {
+                Text("show modal")
+            }
+
+        }
+        .padding()
+        
+    }
+}
+
+
+
+class PresentingViewController: UIViewController, UIViewControllerTransitioningDelegate {
+    
+    var transitionStyle: PopupDialogTransitionStyle = .zoomIn
+    
+    func presentationController(forPresented presented: UIViewController, presenting: UIViewController?, source: UIViewController) -> UIPresentationController? {
+        let presentationController = PresentationController(presentedViewController: presented, presenting: source)
+        return presentationController
+    }
+
+    func animationController(forPresented presented: UIViewController, presenting: UIViewController, source: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+
+        var transition: TransitionAnimator
+        switch transitionStyle {
+        case .bounceUp:
+            transition = BounceUpTransition(direction: .in)
+        case .bounceDown:
+            transition = BounceDownTransition(direction: .in)
+        case .zoomIn:
+            transition = ZoomTransition(direction: .in)
+        case .fadeIn:
+            transition = FadeTransition(direction: .in)
+        }
+
+        return transition
+    }
+
+    func animationController(forDismissed dismissed: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        var transition: TransitionAnimator
+        switch transitionStyle {
+        case .bounceUp:
+            transition = BounceUpTransition(direction: .out)
+        case .bounceDown:
+            transition = BounceDownTransition(direction: .out)
+        case .zoomIn:
+            transition = ZoomTransition(direction: .out)
+        case .fadeIn:
+            transition = FadeTransition(direction: .out)
+        }
+
+        return transition
+    }
+
+    func presentPopup() {
+        let popupContentViewController = UIHostingController(rootView: DialogView())
+        popupContentViewController.modalPresentationStyle = .custom
+        popupContentViewController.transitioningDelegate = self
+        popupContentViewController.view.backgroundColor = .clear
+        UIApplication.shared.present(popupContentViewController)
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+            popupContentViewController.dismiss(animated: true)
+        }
+    }
+}
+
+struct DialogView: View {
+    
+    var body: some View {
+        ZStack {
+            
+            Text("hello world").foregroundColor(.white)
+                .onTapGesture {
+                    UIApplication.shared.topMostViewController?.dismiss(animated: true)
+                }
+        }
+        .frame(width: 200, height: 200)
+        .background(Color.black)
+
+    }
+}
+
+extension UIApplication {
+    
+    var firstWindow: UIWindow? {
+        let scenes: Set<UIScene> = UIApplication.shared.connectedScenes
+        let windowScene: UIWindowScene? = scenes.first as? UIWindowScene
+        return windowScene?.windows.first
+    }
+    
+    var topMostViewController: UIViewController? {
+        var topMostVC = self.firstWindow?.rootViewController
+        while let presentedVC = topMostVC?.presentedViewController {
+            topMostVC = presentedVC
+        }
+        return topMostVC
+    }
+    
+    func present(_ vc: UIViewController) {
+        self.topMostViewController?.present(vc, animated: true)
+    }
+    
+}
---
+++ dialogs/dialogsApp.swift
@@ -0,0 +1,17 @@
+//
+//  dialogsApp.swift
+//  dialogs
+//
+//  Created by paige shin on 2023/06/02.
+//
+
+import SwiftUI
+
+@main
+struct dialogsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
